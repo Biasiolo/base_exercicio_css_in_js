8d8b386bc5f89d3b126c48f861bb6501bd151bc6
Biasiolo <biasiolorafael@gmail.com>
2024-08-15 13:46:30 -0300

    initial

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,14 @@
-import styles from './Hero.module.css'
+// src/components/Hero/index.tsx
+import { HeroForm, HeroContent, HeroTitle } from './Hero.styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <HeroForm>
+    <HeroContent className="container">
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </HeroContent>
+  </HeroForm>
 )
 
 export default Hero
